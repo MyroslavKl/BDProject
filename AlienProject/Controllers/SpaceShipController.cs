fb52dd783fbd305df5b052f057bb905e1119b5d5
Myroslav <muroslavkl@gmail.com>
2023-11-27 16:04:37 +0200

    Add an opportunity to find spaceships which was visited by scecific human

--- AlienProject/Controllers/SpaceShipController.cs
+++ AlienProject/Controllers/SpaceShipController.cs
@@ -1,5 +1,6 @@
 ﻿using AlienProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlienProject.Controllers
 {
@@ -15,5 +16,48 @@ namespace AlienProject.Controllers
             var spaceShips = _context.Spaceshipps.ToList();
             return View(spaceShips);
         }
+        [HttpGet]
+        public IActionResult FindPeople() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult FindPeople(string humanName, DateTime fromDate, DateTime toDate) {
+            var data = GetCollection();
+            var spaceshipsVisited = data
+            .Where(visit => visit.HumanName == humanName && visit.SpaceshipVisitDate >= fromDate && visit.SpaceshipVisitDate <= toDate)
+            .Select(visit => visit.SpaceshipName)
+            .ToList();
+            return View(spaceshipsVisited);
+        }
+
+
+        public IEnumerable<SpaceshipViewModel> GetCollection()
+        {
+            var query = from visit in _context.SpaceshippVisits
+                        join spaceship in _context.Spaceshipps on visit.SpaceshipId equals spaceship.SpaceshipId
+                        join human in _context.Humans on visit.HumanId equals human.HumanId
+                        select new
+                        {
+                            visit.SpaceshipVisitId,
+                            visit.SpaceshipId,
+                            visit.SpaceshipVisitDate,
+                            visit.HumanId,
+                            HumanName = human.Name,
+                            SpaceshipName = spaceship.SpaceshipName
+                        };
+
+            IEnumerable<SpaceshipViewModel> result = query.Select(res => new SpaceshipViewModel
+            {
+                SpaceshipVisitId = res.SpaceshipVisitId,
+                SpaceshipId = res.SpaceshipId,
+                SpaceshipVisitDate = res.SpaceshipVisitDate,
+                SpaceshipName = res.SpaceshipName,
+                HumanId = res.HumanId,
+                HumanName = res.HumanName
+
+            });
+            return result;
+        }
+
     }
 }
